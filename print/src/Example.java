fbe788b1437d0d3307af05a4a12556857754ef8b
heikov123 <heiko.valli@khk.ee>
2019-10-07 10:21:09 +0300

    Week 1 - esimesed 3 ülesannet

---
+++ print/src/Example.java
@@ -0,0 +1,6 @@
+public class Example {
+    public static void main(String[] args) {
+        // program code
+        System.out.print("Text to be printed");
+    }
+}
---
+++ print/src/Exercise2.java
@@ -0,0 +1,6 @@
+public class Exercise2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.print("And all the people of the world");
+    }
+}

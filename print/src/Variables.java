7ea6eae3546a59436c73a47ed2fa39ed73a8b7a2
heikov123 <heiko.valli@khk.ee>
2019-10-07 12:04:20 +0300

    Week 1 - Variables ül 1

--- print/src/Variables.java
+++ print/src/Variables.java
@@ -1,5 +1,24 @@
 public class Variables {
     public static void main(String[] args){
+        String chick = "Chickens:";
+        int chickNr = 3;
+        String bacon = "Bacon (kg)";
+        double baconNr = 5.5;
+        String tractor = "A tractor:";
+        String none = "There is none!";
+        String nutshell = "In a nutshell:";
 
+
+        System.out.println(chick);
+        System.out.println(chickNr);
+        System.out.println(bacon);
+        System.out.println(baconNr);
+        System.out.println(tractor);
+        System.out.println(none);
+        System.out.println("");
+        System.out.println(nutshell);
+        System.out.println(chickNr);
+        System.out.println(baconNr);
+        System.out.println(none);
     }
 }

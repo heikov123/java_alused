e66ec338a4fc8a9128632a5dddf3247f4c1b8a8e
heikov123 <heiko.valli@khk.ee>
2019-10-08 13:53:44 +0300

    Week 1 - Exercise 6: Addition

---
+++ print/src/exercise6.java
@@ -0,0 +1,7 @@
+public class exercise6 {
+    public static void main(String[] args){
+        int numberOne = 5123;
+        int numberTwo = 214;
+        System.out.println(numberOne + numberTwo);
+    }
+}

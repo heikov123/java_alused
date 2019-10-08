2cb39bc8ecc6d9fd311e2a37ebf1a9797aada26f
heikov123 <heiko.valli@khk.ee>
2019-10-08 13:55:49 +0300

    Week 1 - Exercise 7: Multiplication

---
+++ print/src/Exercise7.java
@@ -0,0 +1,7 @@
+public class Exercise7 {
+    public static void main(String[] args){
+        int numberOne = 25;
+        int numberTwo = 15;
+        System.out.println(numberOne * numberTwo);
+    }
+}

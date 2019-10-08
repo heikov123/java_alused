286129007963fb89ee2eb0bbc2c8338614db1d87
heikov123 <heiko.valli@khk.ee>
2019-10-08 13:44:55 +0300

    Week 1 - Exercise 5: Seconds in a year

---
+++ print/src/Calculation.java
@@ -0,0 +1,10 @@
+public class Calculation {
+    public static void main(String[] args){
+        int days = 365;
+        int hours = 24;
+        int minutes = 60;
+        int seconds = 60;
+        int result = days * hours * minutes * seconds;
+        System.out.println(result);
+    }
+}

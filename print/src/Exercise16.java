e1ff6fd8e5c77472a1ad49b21f6abf82e7697687
heikov123 <heiko.valli@khk.ee>
2019-10-10 14:21:34 +0300

    Week 1 - Exercise 16: Even or odd?

--- print/src/Exercise16.java
+++ print/src/Exercise16.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Exercise16 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type a number: ");
+        int number = Integer.parseInt(reader.nextLine());
+
+        if (number % 2 == 0) {
+            System.out.print("Number " + number + " is even.");
+        }   else {
+            System.out.print("Number " + number + " is odd.");
+        }
+    }
 }

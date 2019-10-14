ee7c836371a65d4252360dfcb31c49a954d5abda
heikov123 <heiko.valli@khk.ee>
2019-10-14 10:06:01 +0300

    Week 2 - Exercise 27: From one to a hundred

--- print/src/Exercise27.java
+++ print/src/Exercise27.java
@@ -4,6 +4,11 @@ public class Exercise27 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        int number = 0;
 
+        while (number <= 100) {
+            System.out.println(number);
+            number++;
+        }
     }
 }

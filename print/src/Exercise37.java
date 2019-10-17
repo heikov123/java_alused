b985f585d55dcbe3e3b2954b178bab8470d06a7d
heikov123 <heiko.valli@khk.ee>
2019-10-17 19:26:47 +0300

    Week 2 - Exercise 37: Printing out text

---
+++ print/src/Exercise37.java
@@ -0,0 +1,9 @@
+public class Exercise37 {
+    public static void main(String[] args) {
+        printText();
+    }
+
+    public static void printText() {
+        System.out.print("In the beginning there were the swamp, the hoe and Java");
+    }
+}

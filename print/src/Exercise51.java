9936d348f226467dd23fa02244f255a043351f5e
heikov123 <heiko.valli@khk.ee>
2019-10-23 08:50:06 +0300

    Week 3 - Exercise 51: Separating characters

---
+++ print/src/Exercise51.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Exercise51 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+        int i = 0;
+
+        for (i = 0; i < name.length(); i++) {
+            if (i >= name.length()) {
+                return;
+            } else if (name.length() < 3) {
+                return;
+            }
+            System.out.println(i + ". character: " + name.charAt(i));
+        }
+    }
+}

630497525574275b59e2bf7268a4223c331bc860
heikov123 <heiko.valli@khk.ee>
2019-11-04 15:51:27 +0200

    Week 3 - Exercise 61: Amount of items in a list

---
+++ print/src/Exercise61.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Exercise61 {
+    public static void print(ArrayList<String> printed) {
+        for (String word : printed) {
+            System.out.println( word );
+        }
+    }
+    public static int countItems(ArrayList<String> list) {
+        int i = 0;
+        for (String word : list) {
+            i++;
+        }
+        return i;
+    }
+    public static void removeFirst(ArrayList<String> list) {
+        list.remove(0);
+    }
+    public static void main(String[] args) {
+        ArrayList<String> programmingLanguages = new ArrayList<String>();
+        programmingLanguages.add("Pascal");
+        programmingLanguages.add("Java");
+        programmingLanguages.add("Python");
+        programmingLanguages.add("Ruby");
+        programmingLanguages.add("C++");
+
+        print(programmingLanguages);
+
+        removeFirst(programmingLanguages);
+
+        System.out.println();
+        print(programmingLanguages);
+    }
+}
